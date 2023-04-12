cfdf8300cafd02cc2327cd25411b0b6a1ec8aaf1
Henry J. Perez <henryjosiasperezrangel@gmail.com>
2023-04-12 09:28:36 -0400

    solving the between two sets challenge (but with the help of a guide 😔)

--- 13.between_two_sets/index.ts
+++ 13.between_two_sets/index.ts
@@ -1,12 +1,34 @@
-function getTotalX(a, b) {
-	let potential = [], final = [];
-	for (let i = a[a.length - 1]; i <= b[0]; i++) {
-		if (a.every(x => i % x === 0)) potential.push(i);
+function getTotalX(a: number[], b: number[]): number {
+	let multiple = 0, gcd = b[0], lcm = a[0];
+	
+	for (let num of a) {
+		lcm = setLCM(lcm, num);
 	}
-	for (let i = 0, len = potential.length; i < len; i++) {
-		if (b.every(x => x % potential[i] === 0)) final.push(potential[i]);
+	for (let num of b) {
+		gcd = setGCD(gcd, num);
 	}
-	return final.length;
+	let result = 0;
+	while (multiple <= gcd) {
+		multiple += lcm;
+
+		if (gcd % multiple == 0) {
+			result ++;
+		}
+	}
+	return result;
+}
+function setGCD(n1: number, n2: number): number {
+	if (n2 === 0) {
+		return n1;
+	}
+	return setGCD(n2, n1 % n2);
+}
+function setLCM(n1: number, n2: number): number {
+	if (n1 == 0 || n2 == 0) {
+		return 0;
+	}
+	const gcd = setGCD(n1, n2);
+	return Math.abs(n1 * n2) / gcd;
 }
 
 console.log(getTotalX([2, 4], [16, 32, 96]));
